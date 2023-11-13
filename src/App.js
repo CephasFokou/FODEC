18178014b092daa5d1a11759301d02f86d3b021e
Cephas F <cephasfokou@gmail.com>
2023-11-13 18:42:52 +0100

    Default redirect home page

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<Home />} />
-                <Route path='/login' element={<Login />} />
+                <Route index element={<Login />} />
+                <Route path='/home' element={<Home />} />
                 <Route path='/signup' element={<Signup />} />
                 <Route path="/*" element={<NotFound />} />
             </Routes>
